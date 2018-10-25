1f0409c067ec53f3c3a3fe5112b8d4ced2a366be
Melissa Patxot <melissapatxot@gmail.com>
2018-10-25 18:47:19 -0400

    Updated gallery

--- gallery.js
+++ gallery.js
@@ -1,20 +1,31 @@
-$("#landscapes").on("click", function){
+$("#cat1").on("click", function(){
 	$("#landscapes").css("display", "inline-block")
 	$("#portaits, #religion, #abstracts").css("display", "none")
-}
+})
 
-$("#portraits").on("click", function){
+$("#cat2").on("click", function(){
 	$("#portraits").css("display", "inline-block")
 	$("#landscapes, #religion, #abstracts").css("display", "none")
-}
+})
 
-$("#religion").on("click", function){
+$("#cat3").on("click", function(){
 	$("#religion").css("display", "inline-block")
 	$("#portaits, #landscapes, #abstracts").css("display", "none")
-}
+})
 
-$("abstracts").on("click", function){
+$("#cat4").on("click", function(){
 	$("#abstracts").css("display", "inline-block")
 	$("#portaits, #religion, #landscapes").css("display", "none")
-}
+})
 
+
+this.onclick = function(e){
+	$("#box").css("display", "block")
+	$("#pic2").css("width", "700px")
+	var address = e.target.getAttribute("src")
+	$("#pic2").attr("src", address)
+	$("#box").css("position", "fixed")
+	$("#box").css("top", "100px")
+	$("#box").css("left", "10%")
+	$("#box").css("border", "2px solid black")
+}
