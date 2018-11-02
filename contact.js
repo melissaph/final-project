c9e320a8e8e83520dd1c24a1865c49f4bd8f4ed6
Melissa Patxot <melissapatxot@gmail.com>
2018-11-01 20:35:15 -0400

    Added contact page

---
+++ contact.js
@@ -0,0 +1,6 @@
+var button = document.querySelector("#btn")
+
+button.onclick = function(){
+	var response = document.querySelector("#response")
+	response.innerHTML = "Thank you! We will contact you in 2-4 business days."
+}
---
+++ vinicio-books.js
@@ -0,0 +1,29 @@
+$("#front1").mouseover(function(){
+	$("#front1").css("display", "none")
+	$("#back1").css("display", "inline-block")
+})
+
+$("#back1").mouseleave(function(){
+	$("#back1").css("display", "none")
+	$("#front1").css("display", "inline-block")
+})
+
+$("#front2").mouseover(function(){
+	$("#front2").css("display", "none")
+	$("#back2").css("display", "inline-block")
+})
+
+$("#back2").mouseleave(function(){
+	$("#back2").css("display", "none")
+	$("#front2").css("display", "inline-block")
+})
+
+$("#front3").mouseover(function(){
+	$("#front3").css("display", "none")
+	$("#back3").css("display", "inline-block")
+})
+
+$("#back3").mouseleave(function(){
+	$("#back3").css("display", "none")
+	$("#front3").css("display", "inline-block")
+})
